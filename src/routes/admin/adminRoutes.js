461d064004712780f4917c032650c2772b20696c
BrunaCelestino <brunaccelestino@gmail.com>
2022-07-19 23:47:24 -0300

    feat: create admin routes

---
+++ src/routes/admin/adminRoutes.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = express.Router();
+
+const controller = require('../../controllers/admin/adminController');
+
+const { validateEmailPasswordUsername, hashPassword } = require('../../helpers/authentication');
+
+router.post('/new', validateEmailPasswordUsername, hashPassword, controller.createNewAdmin);
+router.put('/update/:id', validateEmailPasswordUsername, hashPassword, controller.updateAdmin);
+router.delete('/delete/:id', controller.deleteAdmin);
+router.get('/find-admin-by-email', controller.getOneAdminByEmail);
+router.get('/find-all-admins', controller.getAllAdmins);
+router.get('/find-admin/:id', controller.getOneAdminById);
+
+module.exports = router;
